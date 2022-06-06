0c1705892cbccef2352b8a5ce8beb1317150e9d8
Nilesh <Nileshhkr@gmail.com>
2022-06-07 00:52:12 +0530

    Prototye script table

--- Bu.py
+++ Bu.py
@@ -1,5 +1,7 @@
+from multiprocessing.sharedctypes import Value
 from bs4 import BeautifulSoup as Soup
 import requests
+import json
 
 # from script.scraper import URL
 Bu_facts = {}
@@ -12,12 +14,21 @@ factsCategories = soup.find(class_ = "facts-categories")
 
 categories = factsCategories.find_all('h5')
 
-facts_list = factsCategories.find(class_ = "row list").text.strip()
+facts_list = factsCategories.find_all(class_ = "row list")
 
-for categori in categories :
+for categori , facts in zip(categories, facts_list):
+    i = []
     formatCategori = str(categori).replace("<h5>","").replace("</h5>","")
-    Bu_facts[formatCategori] = None
-    print(formatCategori)
-print(facts_list.replace(",",""))    
-print(Bu_facts)
 
+    
+    fact = facts.find_all(class_ = 'list-item')
+    for j in fact:
+        var = []
+        var.append(j.find(class_="text").text)
+        var.append(j.find(class_="value").text)
+        i.append(var)
+    Bu_facts[formatCategori] = i
+
+
+with open('facts.json', 'w') as f:
+    json.dump(Bu_facts,f, indent=2)
